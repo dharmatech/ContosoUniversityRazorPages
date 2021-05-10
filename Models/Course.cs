54bef04d128f2c508abf4094285560d7993a50b9
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-10 10:25:48 -0700

    Course - Enrollments navigation property

--- Models/Course.cs
+++ Models/Course.cs
@@ -12,5 +12,7 @@ namespace ContosoUniversity.Models
         public int CourseID { get; set; }
         public string Title { get; set; }
         public int Credits { get; set; }
+
+        public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
