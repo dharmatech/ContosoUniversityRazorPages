2ca442a49685643126acc8cf064388cb7ce21c91
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-10 10:30:22 -0700

    CMD install dotnet-aspnet-codegenerator

--- notes.ps1
+++ notes.ps1
@@ -16,3 +16,16 @@ dotnet new gitignore
 # ----------------------------------------------------------------------
 
 dotnet new webapp
+
+# ----------------------------------------------------------------------
+
+dotnet add package Microsoft.EntityFrameworkCore.SQLite -v 5.0.0-*
+dotnet add package Microsoft.EntityFrameworkCore.SqlServer -v 5.0.0-*
+dotnet add package Microsoft.EntityFrameworkCore.Design -v 5.0.0-*
+dotnet add package Microsoft.EntityFrameworkCore.Tools -v 5.0.0-*
+dotnet add package Microsoft.VisualStudio.Web.CodeGeneration.Design -v 5.0.0-*
+dotnet add package Microsoft.AspNetCore.Diagnostics.EntityFrameworkCore -v 5.0.0-*  
+
+# ----------------------------------------------------------------------
+
+dotnet tool install --global dotnet-aspnet-codegenerator
