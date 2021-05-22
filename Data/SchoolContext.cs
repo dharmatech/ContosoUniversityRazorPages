d67d1dbf2f27a11066c05dc9d3443ed2984de4af
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-22 00:48:05 -0700

    Update SchoolContext

--- Data/SchoolContext.cs
+++ Data/SchoolContext.cs
@@ -17,12 +17,24 @@ namespace ContosoUniversity.Data
         public DbSet<Student> Students { get; set; }
         public DbSet<Enrollment> Enrollments { get; set; }
         public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<OfficeAssignment> OfficeAssignments { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<Course>().ToTable("Course");
-            modelBuilder.Entity<Enrollment>().ToTable("Enrollment");
-            modelBuilder.Entity<Student>().ToTable("Student");
+            modelBuilder.Entity<Student>().ToTable(nameof(Student));
+
+            modelBuilder.Entity<Enrollment>().ToTable(nameof(Enrollment));
+
+            modelBuilder.Entity<Course>().ToTable(nameof(Course))
+                .HasMany(course => course.Instructors).WithMany(instructor => instructor.Courses);
+
+            modelBuilder.Entity<Department>().ToTable(nameof(Department));
+
+            modelBuilder.Entity<Instructor>().ToTable(nameof(Instructor));
+
+            modelBuilder.Entity<OfficeAssignment>().ToTable(nameof(OfficeAssignment));
         }
     }
 }
