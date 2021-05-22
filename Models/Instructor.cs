83569342453b7132e00548d638c741aad8f60655
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-21 18:29:40 -0700

    Models - Instructor and OfficeAssignment

---
+++ Models/Instructor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Models
+{
+    public class Instructor
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+
+        [Required]
+        [Column("FirstName")]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        public string FullName => LastName + ", " + FirstMidName;
+
+        public ICollection<Course> Courses { get; set; } // navigation
+        public OfficeAssignment OfficeAssignment { get; set; } // navigation
+    }
+}
