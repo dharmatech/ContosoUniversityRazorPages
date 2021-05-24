bbbe8e3c94a57e01fc8677c17aa3c46a1d46506f
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-24 12:32:46 -0700

    Utility class - GetLastChars

---
+++ Utility.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity
+{
+    public static class Utility
+    {
+        public static string GetLastChars(Guid token)
+        {
+            return token.ToString().Substring(token.ToString().Length - 3);
+        }
+    }
+}
