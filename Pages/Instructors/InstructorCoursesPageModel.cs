bdbaa5f440d54d10e73c58b6c7471001a73752a9
Eduardo Cavazos <wayo.cavazos@gmail.com>
2021-05-23 22:58:36 -0700

    InstructorCoursesPageModel

---
+++ Pages/Instructors/InstructorCoursesPageModel.cs
@@ -0,0 +1,36 @@
+﻿using ContosoUniversity.Data;
+using ContosoUniversity.Models;
+using ContosoUniversity.Models.SchoolViewModels;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ContosoUniversity.Pages.Instructors
+{
+    public class InstructorCoursesPageModel : PageModel
+    {
+        public List<AssignedCourseData> AssignedCourseDataList;
+
+        public void PopulateAssignedCourseData(SchoolContext context, Instructor instructor)
+        {
+            var allCourses = context.Courses;
+
+            var instructorCourses = new HashSet<int>(
+                instructor.Courses.Select(course => course.CourseID));
+
+            AssignedCourseDataList = new List<AssignedCourseData>();
+
+            foreach (var course in allCourses)
+            {
+                AssignedCourseDataList.Add(new AssignedCourseData() 
+                {
+                    CourseID = course.CourseID,
+                    Title = course.Title,
+                    Assigned = instructorCourses.Contains(course.CourseID)
+                });
+            }
+        }
+    }
+}
